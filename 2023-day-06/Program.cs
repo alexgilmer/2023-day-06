0be7a146cdfd51129191de20d7f0e12c06bfaf81
Alex Gilmer <alex.gilmer.business@gmail.com>
2024-08-14 14:21:22 -0500

    Solve part 2

--- 2023-day-06/Program.cs
+++ 2023-day-06/Program.cs
@@ -9,7 +9,14 @@ internal class Program
         (78, 1215),
         (80, 1524)
     ];
+
+    private static (int, long) Race2 = (53717880, 275118112151524);
     static void Main(string[] args)
+    {
+        SolvePart2();
+    }
+
+    private static void SolvePart1()
     {
         int[] outcomes = new int[Races.Length];
         for (int i = 0; i < Races.Length; i++)
@@ -33,5 +40,35 @@ internal class Program
             Console.WriteLine(outcome);
         }
         Console.WriteLine(outcomes.Aggregate(1, (i, j) => i * j));
+    }
+
+    private static void SolvePart2()
+    {
+        // this input is _much_ larger
+        // a simple linear search might take a while
+        // a binary search could more quickly find the right answer
+        // .... or we treat this as an order-2 polynomial equation
+        // and apply calculus to solve for the two "zeroes"
+
+        DoALinearSearch();
+
+        // in reality, the linear search still solved it in under 1s of run time. 
+    }
+
+    private static void DoALinearSearch()
+    {
+        long result = 0;
+        for (long j = 0; j < Race2.Item1; j++)
+        {
+            long speed = j;
+            long time = Race2.Item1 - j;
+            long distance = speed * time;
+            if (distance > Race2.Item2)
+            {
+                result++;
+            }
+        }
+
+        Console.WriteLine(result);
     }
 }
